dec803f9355d54ca02ef7530f42896cefdb413da
Curry5004 <yunjin5004@naver.com>
2025-02-13 07:27:20 +0900

    * 25. 02. 13
    create package

---
+++ src/main/java/hello/thymleafbasic/KyhSpringMvc2ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package hello.thymleafbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyhSpringMvc2ThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KyhSpringMvc2ThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/thymleafbasic/KyhSpringMvc2ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.thymleafbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyhSpringMvc2ThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
